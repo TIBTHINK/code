fc2ef0fc180ca81ba437c0a24cc0d29a8ee9c9f6
TIBTHINK <bent23@gmail.com>
2019-04-06 11:03:24 -0400

    making index files for all

---
+++ c++/index.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+            printf(“\n\nWelcome to my Homepage!!\n\n”);
+
+            return 0;
+
+}
+
+(i). C++ program code:
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+            cout<<“\n\nHello World,\nWelcome to my first C ++ program on Ubuntu Linux\n\n”<<endl;
+
+            return 0;
+
+}
