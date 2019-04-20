0bc1001cca6ed013d7e7c4b2b8d16177422b3ee4
TIBTHINK <bent23jam@gmail.com>
2019-04-19 22:18:30 -0400

    dsfjghvkjfgbkdfg
    i did it

--- python/index.py
+++ python/index.py
@@ -1,9 +1,14 @@
+AiName = "SCP-4032"
+
 print("hello what is your name")
 name = input('Enter your name: ')
 print("hello there " + name)
+print()
 print("And how old are you")
 age = input('How old are you: ')
 print()
 print("so your name is " + name)
+print()
 print("and your " + age)
-print("well my name is also " + name)
+print()
+print("well my name is also " + AiName)
