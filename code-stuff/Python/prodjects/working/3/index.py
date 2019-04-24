9590db82d7d948326f3a6a9b6333d147e68b52f1
TIBTHINK <bent23jam@gmail.com>
2019-04-23 20:46:38 -0400

    ducks part

--- code-stuff/Python/prodjects/working/3/index.py
+++ code-stuff/Python/prodjects/working/3/index.py
@@ -1,3 +1,38 @@
 import os
 import time
 import random
+from time import sleep
+import sys
+
+
+if os.geteuid() != 0:
+    exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo' then run the script.\nExiting.")
+
+print("Fantastic, lets get started")
+time.sleep(3)
+os.system("clear")
+print("updating apt packages")
+time.sleep(2)
+os.system("apt-get update")
+time.sleep(2)
+os.system("clear")
+print("now upgrading any packages")
+time.sleep(2)
+os.system("apt-get full-upgrade -y")
+
+print()
+
+
+
+
+lines = ["it is done"]
+
+for line in lines:
+    for c in line:
+        print(c, end='')
+        sys.stdout.flush()
+        sleep(0.1)
+    print('')
+
+time.sleep(4)
+os.system("clear")
