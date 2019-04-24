79b29303e152571e779fe7593bca0485701eeedb
TIBTHINK <bent23jam@gmail.com>
2019-04-23 20:59:15 -0400

    python script that updates your apt packeges

--- code-stuff/Python/prodjects/working/3/index.py
+++ code-stuff/Python/prodjects/working/3/index.py
@@ -11,27 +11,31 @@ if os.geteuid() != 0:
 print("Fantastic, lets get started")
 time.sleep(3)
 os.system("clear")
-print("updating apt packages")
+print("#updating apt packages")
 time.sleep(2)
 os.system("apt-get update")
 time.sleep(2)
 os.system("clear")
-print("now upgrading any packages")
+print("#now upgrading any packages")
 time.sleep(2)
 os.system("apt-get full-upgrade -y")
+time.sleep(2)
+os.system("clear")
+print("#time to remove some usless packages")
+os.system("apt-get autoremove -y")
+time.sleep(2)
+os.system("clear")
 
-print()
 
 
 
-
-lines = ["it is done"]
+lines = [";) LOL"]
 
 for line in lines:
     for c in line:
         print(c, end='')
         sys.stdout.flush()
-        sleep(0.1)
+        sleep(1)
     print('')
 
 time.sleep(4)
