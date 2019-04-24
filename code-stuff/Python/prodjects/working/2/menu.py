febac9b42197d86aed91156602c530e5fe91481a
TIBTHINK <bent23jam@gmail.com>
2019-04-24 19:45:36 -0400

    gonna make a logo in css brb

--- code-stuff/Python/prodjects/working/2/menu.py
+++ code-stuff/Python/prodjects/working/2/menu.py
@@ -1,26 +1,79 @@
 import os
-import time
+from time import sleep
+import platform
+import sys
+
+
+logo = ("""
+
+           /$$
+          |__/
+  /$$$$$$  /$$  /$$$$$$  /$$   /$$ /$$   /$$
+ /$$__  $$| $$ /$$__  $$|  $$ /$$/| $$  | $$
+| $$  \ $$| $$| $$  \ $$ \  $$$$/ | $$  | $$
+| $$  | $$| $$| $$  | $$  >$$  $$ | $$  | $$
+| $$$$$$$/| $$|  $$$$$$/ /$$/\  $$|  $$$$$$$
+| $$____/ |__/ \______/ |__/  \__/ \____  $$
+| $$                               /$$  | $$
+| $$                              |  $$$$$$/
+|__/                               \______/
+--------------------------------------------
+""")
+
+logo2 = ("""
+
+           /$$
+          |__/
+  /$$$$$$  /$$  /$$$$$$  /$$   /$$ /$$   /$$
+ /$$__  $$| $$ /$$__  $$|  $$ /$$/| $$  | $$
+| $$  \ $$| $$| $$  \ $$ \  $$$$/ | $$  | $$
+| $$  | $$| $$| $$  | $$  >$$  $$ | $$  | $$
+| $$$$$$$/| $$|  $$$$$$/ /$$/\  $$|  $$$$$$$
+| $$____/ |__/ \______/ |__/  \__/ \____  $$
+| $$                               /$$  | $$
+| $$                              |  $$$$$$/
+|__/                               \______/
+--------------------------------------------
+""")
 
 
 if os.geteuid() != 0:
-    exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo' then run the script.\nExiting.")
-    
+    exit("You need to have root privileges to run this script.\nPlease try again, Run 'sudo !!' and run the script.\nExiting.")
+os.system("clear")
+print(logo)
+print()
+
+
 ans=True
 while ans:
+    print()
     print ("""
     1.Update the PI
-    2.Not used yet
-    3.Not used yet
-    4.Not used yet
+    2.Check for updates
+    3.Proxy
+    4.Exit
     """)
-    ans=raw_input("What would you like to do? ")
+    ans=input("What would you like to do? ")
     if ans=="1":
       os.system("apt-get update && apt-get upgrade -y")
+      print(logo2)
     elif ans=="2":
-      print("\n Student Deleted")
+      print("\n in the workings come back later")
+      sleep(2)
+      os.system("clear")
+      print(logo2)
     elif ans=="3":
-      print("\n Student Record Found")
+      print("\n In the workings come back later")
+      sleep(2)
+      os.system("clear")
+      print(logo2)
     elif ans=="4":
-      print("\n Goodbye")
+      print("\n Exiting, Thanks for the visit")
+      sleep(2)
+      os.system("clear")
+      sys.exit()
     elif ans !="":
       print("\n Not Valid Choice Try again")
+      sleep(2)
+      os.system("clear")
+      print(logo2)
