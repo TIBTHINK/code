87b57238fd63cd4418c9d48b0882ef3040eab04e
TIBTHINK <bent23jam@gmail.com>
2019-04-28 15:05:28 -0400

    l

--- code-stuff/Python/prodjects/working/2/index.py
+++ code-stuff/Python/prodjects/working/2/index.py
@@ -17,6 +17,3 @@ logo = ("""
 if os.geteuid() != 0:
     exit("You need to have root privileges to run this script.\nPlease try again, Run 'sudo !!' and run the script.\nExiting.")
 os.system("clear")
-
-print("hell0 there.\nWhere do you live?")
-loc = input("address: ")
