914d29e653845e5f6ac0829635e2f7d52559e234
TIBTHINK <bent23jam@gmail.com>
2019-05-02 16:53:08 -0400

    im done

--- code-stuff/Python/prodjects/working/4/index.py
+++ code-stuff/Python/prodjects/working/4/index.py
@@ -5,26 +5,37 @@ print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
-print("5. Exit")
+print("5. Times x to y")
+print("6. Exit")
 choice = int(input("Enter your choice: "))
 os.system("clear")
-if (choice>=1 and choice<=4):
+if (choice>=1 and choice<=5):
     print("Enter two numbers: ")
     x = int(input())
     y = int(input())
     if choice == 1:
     	res = x + y
     	print("Result = ", res)
+
     elif choice == 2:
     	res = x - y
     	print("Result = ", res)
+
     elif choice == 3:
     	res = x * y
     	print("Result = ", res)
-    else:
+
+    elif choice == 4:
     	res = x / y
     	print("Result = ", res)
-elif choice == 5:
+
+    elif choice == 5:
+        res = x ** y
+        print("Result = ", res)
+
+
+
+elif choice == 6:
     exit()
 else:
     print("Wrong input..!!")
